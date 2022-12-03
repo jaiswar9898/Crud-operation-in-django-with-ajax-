2ec560dbc98b40eb7b6648294bb77ab6a1315f2f
jaiswar9898 <pranitajaiswal960@gmail.com>
2022-12-03 07:45:50 -0800

    Ajax

---
+++ autoajaxapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    course = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+        
